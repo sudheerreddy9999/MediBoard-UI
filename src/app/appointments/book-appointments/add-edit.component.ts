d496a1fbddd8732ccf836db0bf8eaa7c06aef6cf
sudheerjanga <sudheerjanga9999@gmail.com>
2024-09-28 00:18:07 +0530

    Appointment page

--- src/app/appointments/book-appointments/add-edit.component.ts
+++ src/app/appointments/book-appointments/add-edit.component.ts
@@ -1,46 +1,103 @@
 import { Component, OnInit, AfterViewInit ,Output,EventEmitter} from '@angular/core';
 import { HttpClient,HttpHeaders } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
 import { DoctorsComponent } from '../doctors/doctors.component';
 import { CommonModule } from '@angular/common';
-import { MatIcon } from '@angular/material/icon';
+import { FormControl, FormGroup, FormsModule,ReactiveFormsModule, Validators } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
 import { CalendarModule } from 'primeng/calendar';
 import { TimeFormatPipe } from '../../pipes/time-format.pipe';
 import flatpickr from 'flatpickr';
 import { format } from 'date-fns';
 import { __param } from 'tslib';
+import { LoaderComponent } from '../../components/loader/loader.component';
+import { ModalComponent } from '../../components/modal/modal.component';
+import { environment } from '../../../environments/environment';
 
 
 @Component({
   selector: 'app-add-edit',
   standalone: true,
-  imports: [CommonModule,DoctorsComponent,FormsModule,CalendarModule,TimeFormatPipe,MatIcon],
+  imports: [CommonModule,DoctorsComponent,FormsModule,CalendarModule,TimeFormatPipe,MatIcon,MatIconModule,FontAwesomeModule,ReactiveFormsModule,LoaderComponent,ModalComponent],
   templateUrl: './add-edit.component.html',
   styleUrls: ['./add-edit.component.css'] // Corrected the property name
 })
-export class AddEditComponent implements OnInit, AfterViewInit {
-  appointmentFormData:any={
-    id: "",
-    doctor_id: "",
-    patient_id: "",
-    appointment_date: "",
-    appointment_time: "",
-    patient_firstname:"",
-    patient_lastname:"",
-    find_doctor:"",
-    email:"",
-    phone_number:"",
-    date_of_bith:"",
-    doctor_name:"",
+export class AddEditComponent implements AfterViewInit {
+  private apiUrl:string = environment.apiBaseUrl;
+  doctor_id=""
+  doctor_name=""
+  openLoader=false;
+  openModalComponnet: boolean = false;
+  modalMessage: string = 'Success';
+  typeOfModal: string = 'failure';
+  AppointmentData = new FormGroup({
+    patient_firstname:new FormControl('sudheer',[
+      Validators.required,
+      Validators.minLength(2),
+      Validators.pattern('^[a-zA-Z ]+$') 
+    ]),
+    patient_lastname:new FormControl('janga',[
+      Validators.required,
+      Validators.minLength(2),
+      Validators.pattern('^[a-zA-Z ]+$') 
+    ]),
+    mobile_number: new FormControl('6303896543', [
+      Validators.required,
+      Validators.pattern('^[0-9]{10}$')
+    ]),
+    patientEmail: new FormControl('sudheerjanga9999@gmail.com', [
+      Validators.required,
+      Validators.minLength(4),
+      Validators.pattern('^[^\\s@]+@[^\\s@]+\\.[^\\s@]+$')
+    ]),
+    dateOfBirth: new FormControl('', []),
+    find_doctor: new FormControl('', []),
+    doctor_name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4)
+    ]),
+    appointmentDate: new FormControl('', [
+      Validators.required,
+    ]),
+    
+    
+  })
+  
+  onSubmit() {
+    console.log("hello 1232 ")
+    if (this.AppointmentData.invalid) {
+      this.AppointmentData.markAllAsTouched();
+      return;
+    }else{
+      this.openLoader=true;
+      const body = {
+        "name": this.AppointmentData.value.patient_firstname,
+        "mobile_number":this.AppointmentData.value.mobile_number,
+        "email":this.AppointmentData.value.patientEmail,
+        "slot_id":this.userSelectedSlot
+      }
+      console.log(body);
+      this.http.post(`${this.apiUrl}/appointments/guest`,body).subscribe({
+        next: (data) => {
+          console.log(data)
+          this.openLoader=false;
+          this.handleCloseModel();
+      },
+        error: (error) => {
+          console.log(error.message)
+          this.openLoader=false;
+          this.openModalComponnet=true
+          this.modalMessage=error.message
+      },
+    complete: () => {
+      console.log('Request completed');
+    }})
+    }
   }
   @Output() closeAppointmentModel = new EventEmitter<boolean>();
   currentDoctorSlots:any[] =[];
   currentDayDoctorSlots:any[] =[];
   userSelectedSlot:any=0;
-
-  ngOnInit(): void {
-    // You can initialize any necessary data here
-  }
   constructor(private http:HttpClient){}
   issearchDoctor:boolean=false;
   ngAfterViewInit(): void {
@@ -64,6 +121,7 @@ export class AddEditComponent implements OnInit, AfterViewInit {
   }
 
 onDateChange(event: Date[] | Date) {
+  console.log("I am here for date Cahnge")
   if (Array.isArray(event)) {
     if (event.length > 0) {
       const selectedDate = event[0];
@@ -107,21 +165,21 @@ onDateChange(event: Date[] | Date) {
   }
   doctorValueChanged(){
     console.log("I gor added Here How are you")
-    console.log(this.appointmentFormData.doctor_id)
+    console.log(this.doctor_id)
   }
   handleAppointmentData(event:any){
-    console.log(event)
     this.issearchDoctor=false;
-    this.appointmentFormData.doctor_id=event.id
-    this.appointmentFormData.doctor_name=event.name
-    console.log(this.appointmentFormData.doctor_id)
+    this.doctor_id=event.id
+    this.doctor_name=event.name
+
+    this.AppointmentData.get('doctor_name')?.setValue(event.name);
     this.getDoctorSlots()
   }
   getDoctorSlots(){
     const headers = new HttpHeaders({
-      doctor_id: this.appointmentFormData.doctor_id
+      doctor_id: this.doctor_id
     });
-   this.http.get<any>('http://localhost:3000/slots',{ headers }).subscribe({
+   this.http.get<any>(`${this.apiUrl}/slots`,{ headers }).subscribe({
     next: (data) => {
       this.currentDoctorSlots = data.slots;
    },
@@ -132,31 +190,14 @@ complete: () => {
   console.log('Request completed');
 }})
 }
-  bookAppointment(){
-    console.log(this.appointmentFormData)
-    const body = {
-      "name": this.appointmentFormData.patient_firstname,
-      "mobile_number":this.appointmentFormData.phone_number,
-      "email":this.appointmentFormData.email,
-      "slot_id":this.userSelectedSlot
-    }
-    console.log(body);
-    this.http.post('http://localhost:3000/appointments/guest',body).subscribe({
-      next: (data) => {
-        console.log(data)
-    },
-      error: (error) => {
-        console.log(error.message)
-    },
-  complete: () => {
-    console.log('Request completed');
-  }})
-}
   selectTimeSlotIf( slot_id:any){
     console.log(slot_id)
     this.userSelectedSlot=slot_id
   }
   handleCloseModel(){
     this.closeAppointmentModel.emit(false);
+  }
+  handleCloseEmitModal(closeModal: boolean) {
+    this.openModalComponnet = closeModal;
   }
 }
--- src/app/appointments/doctors/doctors.component.ts
+++ src/app/appointments/doctors/doctors.component.ts
@@ -3,11 +3,13 @@ import { HttpClient } from '@angular/common/http';
 import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { LoaderComponent } from '../../components/loader/loader.component';
 import { environment } from '../../../environments/environment';
+import { ModalComponent } from '../../components/modal/modal.component';
 @Component({
   selector: 'app-doctors',
   standalone: true,
-  imports: [MatIconModule,CommonModule,FormsModule],
+  imports: [MatIconModule,CommonModule,FormsModule,LoaderComponent,ModalComponent],
   templateUrl: './doctors.component.html',
   styleUrl: './doctors.component.css'
 })
@@ -16,33 +18,36 @@ export class DoctorsComponent implements OnInit {
   doctors: any[] = []; 
   filteredData:any[] =[];
   searchContent:string = ''
+  loaderOn:boolean=true;
+  openModalComponnet:boolean=false
+  modalMessage: string = 'faliure';
+  typeOfModal: string = 'failure';
   @Output() doctorInfo = new EventEmitter<any>();
   @Output() closeDoctorsSearch = new EventEmitter<boolean>();
 
   constructor(private http: HttpClient) {
     this.filteredData = this.doctors
-    console.log(this,this.filteredData)
   }
   ngOnInit(): void {
     this.http.get<any>(`${this.apiUrl}/doctors/all`).subscribe({
       next: (response) => {
+        this.loaderOn = false
         this.doctors = response.doctorData
         this.filteredData=response.doctorData
-        console.log(this.doctors)
-        console.log(response)
     },error:(error)=>{
       console.error('There was an error!', error);
+      this.loaderOn = false
+      this.modalMessage = `${error}`
+      this.openModalComponnet=true
     },   complete: () => {
       console.log('API request completed successfully.'); // Optional: Handle completion if needed
     },}
   )
   }
   onSerach(){
-    console.log("hello How are you")
     this.filteredData = this.doctors.filter(item=>
       item.name.toLowerCase().includes(this.searchContent.toLowerCase())
     )
-    console.log(this.filteredData)
   }
   closeModal(){
     this.closeDoctorsSearch.emit(false);
@@ -53,5 +58,8 @@ export class DoctorsComponent implements OnInit {
       id:doctor_id
     }
     this.doctorInfo.emit(doctorInfo)
+  }
+  handleCloseEmitModal(closeModal: boolean) {
+    this.openModalComponnet = closeModal;
   }
 }
