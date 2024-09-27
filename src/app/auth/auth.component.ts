82270ff77096353412fd0219fe555cd692940803
sudheerjanga <sudheerjanga9999@gmail.com>
2024-09-28 00:17:25 +0530

    appointment changes

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,11 +1,12 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule,ReactiveFormsModule, Validators } from '@angular/forms';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { MatIconModule } from '@angular/material/icon';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AuthService } from '../shared/auth.service';
 import { ModalComponent } from '../components/modal/modal.component';
+import { LoaderComponent } from '../components/loader/loader.component';
 import { environment } from '../../environments/environment';
 import {
   faLock,
@@ -27,7 +28,7 @@ interface AuthResponse {
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [CommonModule, FormsModule, FontAwesomeModule, ModalComponent,MatIconModule],
+  imports: [CommonModule, FormsModule, FontAwesomeModule, ModalComponent,MatIconModule,ReactiveFormsModule,LoaderComponent],
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.css'], // Corrected typo here
 })
@@ -37,6 +38,7 @@ export class AuthComponent implements OnInit {
   @Input() typeOfuser: string = '';
   @Output() userLoggedIn = new EventEmitter<any>();
   @Output() onCloseButtonClicked = new EventEmitter<boolean>();
+  OpenLoader:boolean = false
   userName: string = 'sudheerjanga9999@gmail.com';
   password: string = 'Sudheer@123';
   first_name: string = '';
@@ -52,7 +54,37 @@ export class AuthComponent implements OnInit {
   openModalComponnet: boolean = false;
   modalMessage: string = 'Success';
   typeOfModal: string = 'success';
-  
+  profileForm = new FormGroup({
+    first_name: new FormControl('sudheer', [
+      Validators.required,
+      Validators.minLength(2),  // Ensure at least 2 characters
+      Validators.pattern('^[a-zA-Z ]+$')  // Allow only alphabets and spaces
+    ]),
+    last_name: new FormControl('janga', [
+      Validators.required,
+      Validators.minLength(2),  // Ensure at least 2 characters
+      Validators.pattern('^[a-zA-Z ]+$')  // Allow only alphabets and spaces
+    ]),
+    userName: new FormControl('sudheerjanga9999@gmail.com', [
+      Validators.required,
+      Validators.minLength(4),
+      Validators.pattern('^[^\\s@]+@[^\\s@]+\\.[^\\s@]+$')
+    ]),
+    password: new FormControl('Sudheer@123', this.authIsSignup 
+      ? [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).+$')
+        ]
+      : [
+          Validators.required,
+        ]
+    ),
+    mobile_number: new FormControl('6303896539', [
+      Validators.required,
+      Validators.pattern('^[0-9]{10}$')
+    ])
+  });
 
   ngOnInit(): void {
     if (this.messageFromParent === 'Login') {
@@ -62,6 +94,128 @@ export class AuthComponent implements OnInit {
       this.authIsSignup = true;
       this.isUserLogin = true;
       this.authMessage = "Already a user Login"
+    }
+  }
+  onSubmit() {
+    if (this.authIsSignup) {
+      if (this.profileForm.invalid) {
+        this.profileForm.markAllAsTouched();
+        return;
+      }
+      this.OpenLoader = true;
+      const body = {
+        email: this.profileForm.value.userName,
+        password: this.profileForm.value.password,
+        first_name: this.profileForm.value.first_name,
+        last_name: this.profileForm.value.last_name,
+        mobile_number: this.profileForm.value.mobile_number,
+      };
+      const headers = new HttpHeaders({
+        'Content-Type': 'application/json',
+      });
+
+      this.http
+        .post(`${this.apiUrl}/user/add`, body, { headers: headers })
+        .subscribe({
+          next: (response) => {
+            console.log('Api response is', response);
+            this.authService.login(true);
+            this.OpenLoader=false
+          },
+          error: (error) => {
+            console.error('API Error:', error);
+            this.openModalComponnet = true;
+            this.modalMessage = `${error.error.message} try again `;
+            this.OpenLoader=false
+            this.typeOfModal = 'failure';
+          },
+          complete: () => {
+            console.log('API request completed successfully.');
+          },
+        });
+
+    } else if(!this.authIsSignup) {
+      // For login, validate only email and password
+      const emailControl = this.profileForm.get('userName');
+      const passwordControl = this.profileForm.get('password');
+
+      if (emailControl?.invalid || passwordControl?.invalid) {
+        emailControl?.markAsTouched();
+        passwordControl?.markAsTouched();
+        return;
+      }
+      this.OpenLoader=true
+      console.log('Login Form Submitted', {
+        email: this.profileForm.value,
+        password: this.profileForm.value
+      });
+      if (this.typeOfuser === 'Patient') {
+        const headers = new HttpHeaders({
+          email:this.profileForm?.value.userName||'',
+          password: this.profileForm?.value.password||'',
+          'Content-Type': 'application/json',
+        });
+        console.log(headers)
+        this.http
+          .get<AuthResponse>(`${this.apiUrl}/user/auth`, { headers: headers })
+          .subscribe({
+            next: (response) => {
+              console.log('Api response is', response);
+              this.OpenLoader=false
+              const data = response.data;
+              setTimeout(() => {
+                this.userLoggedIn.emit(data); // Emit the data (assuming you're using EventEmitter)
+              }, 3000);
+              this.openModalComponnet = true;
+              localStorage.setItem('mediboard', JSON.stringify(data)); // Store the data in localStorage
+              this.modalMessage = 'Login Success';
+              this.typeOfModal = 'success';
+            },
+            error: (error) => {
+              this.OpenLoader=false
+              console.error('API Error:', error); // Handle the error
+              this.openModalComponnet = true;
+              this.modalMessage = `${error.error.message} try again `;
+              this.typeOfModal = 'failure';
+            },
+            complete: () => {
+              console.log('API request completed successfully.'); // Optional: Handle completion if needed
+            },
+          });
+      } else if (this.typeOfuser === 'Employee') {
+        const headers = new HttpHeaders({
+          email:this.profileForm?.value.userName||'',
+          password: this.profileForm?.value.password||'',
+          'Content-Type': 'application/json',
+        });
+        this.http
+          .get<AuthResponse>(`${this.apiUrl}/employee/auth`, { headers: headers })
+          .subscribe({
+            next: (response) => {
+              this.OpenLoader=false
+              console.log('Api response is', response);
+              const data = response.data;
+              this.openModalComponnet = true;
+              this.modalMessage = 'Login Success';
+              this.typeOfModal = 'success';
+              setTimeout(() => {
+                this.userLoggedIn.emit(data); // Emit the data (assuming you're using EventEmitter)
+              }, 2000);
+              localStorage.setItem('mediboard', JSON.stringify(data)); // Store the data in localStorage
+              this.authService.login(true);
+            },
+            error: (error) => {
+              console.error('API Error:', error); // Handle the error
+              this.OpenLoader=false
+              this.openModalComponnet = true;
+              this.modalMessage = `${error.error.message} try again `;
+              this.typeOfModal = 'failure';
+            },
+            complete: () => {
+              console.log('API request completed successfully.'); // Optional: Handle completion if needed
+            },
+          });
+      }
     }
   }
 
--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,18 +1,21 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA,AfterViewInit } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SwiperOptions } from 'swiper/types';
 import { AddEditComponent } from '../appointments/book-appointments/add-edit.component';
+
+// Install the Swiper modules
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [CommonModule,AddEditComponent],
+  imports: [CommonModule, AddEditComponent],
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css'],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class HomepageComponent {
-  openBookAppointment:boolean=false;
+
+  openBookAppointment: boolean = false;
+
   slides = [
     { image: '/images/carousel/carousel-1.jpg', title: 'Neurology', description: 'Advanced brain and nerve care.', duration: 3 },
     { image: '/images/carousel/carousel-4.jpg', title: 'Cardiology', description: 'Expert care for heart health.', duration: 4 },
@@ -20,46 +23,13 @@ export class HomepageComponent {
     { image: '/images/carousel/carousel-2.jpg', title: 'Neurology', description: 'Advanced brain and nerve care.', duration: 3 },
     { image: '/images/carousel/carousel-5.jpg', title: 'Pediatrics', description: 'Comprehensive care for children.', duration: 5 }
   ];
-  swiperConfig: SwiperOptions = {
-    slidesPerView: 1,
-    spaceBetween: 20,
-    loop: true,
-    autoplay: {
-      delay: 3000, 
-      disableOnInteraction: false
-    },
-    pagination: {
-      clickable: true
-    },
-    navigation: true,
-    breakpoints: {
-      640: { slidesPerView: 1 },
-      768: { slidesPerView: 2 },
-      1024: { slidesPerView: 3 }
-    }
-  };
-  
-  ngAfterViewInit() {
-  }
-  onSwiper(swiper: any) {
-    // This method will be called when the swiper instance is ready
-    console.log("Helllo i am inside swiper")
-    swiper.autoplay.start(); // Ensure autoplay starts when the Swiper is initialized
+
+  bookAppointmentClicked() {
+    console.log('Book Appointment is Clicked');
+    this.openBookAppointment = !this.openBookAppointment;
   }
 
-
-  onSlideChange(swiper: any) {
-    const currentIndex = swiper.realIndex;
-    const duration = this.slides[currentIndex]?.duration || 3000; // Default to 3000ms if no duration is set
-    swiper.params.autoplay.delay = duration * 1000; // Set delay in milliseconds
-    swiper.autoplay.start(); // Restart autoplay with new delay
-  }
-
-  bookAppointmentClicked(){
-    console.log("Book Appointment is    Clciked")
-    this.openBookAppointment=!this.openBookAppointment;
-  }
-  handleCloseAppointment(){
+  handleCloseAppointment() {
     this.openBookAppointment = false;
   }
 }
